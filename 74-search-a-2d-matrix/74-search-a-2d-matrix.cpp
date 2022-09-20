dc11ff002f7c214579f710ef1b4664ddbc4d17a6
Mohamed Bakr <212284@csi.edu.eg>
2022-09-21 00:30:39 +0200

    Time: 5 ms (69.46%), Space: 9.6 MB (51.28%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        for(int i = 0; i < matrix.size(); i++) {
-            for(int j = 0; j < matrix[0].size(); j++) {
-                if(matrix[i][j] == target) {
+            int row_len = matrix.size(), col_len = matrix[0].size();
+            int l = 0, r = row_len * col_len;
+            while(l < r) {
+                int md = (l+r)/2;
+                if(matrix[md / col_len][md % col_len] == target) {
                     return true;
+                } else if(matrix[md / col_len][md % col_len] < target) {
+                    l = md + 1;
+                } else {
+                    r = md;
                 }
             }
-        }
         return false;
 }
 };

2eebdde09b784bf2f629afb3f366ed2f1d38e594
Mohamed Bakr <212284@csi.edu.eg>
2022-10-10 22:34:41 +0200

    Time: 34 ms (5.11%), Space: 20 MB (5.42%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -1,8 +1,71 @@
 class Solution {
 public:
+void merge(vector<int>& arr, int left, int mid,
+           int right)
+{
+    long long subArrayOne = mid - left + 1;
+    long long subArrayTwo = right - mid;
+
+    vector<int> L(subArrayOne);
+    vector<int> R(subArrayTwo);
+ 
+
+    for (long long i = 0; i < subArrayOne; i++)
+        L[i] = arr[left + i];
+    for (long long j = 0; j < subArrayTwo; j++)
+        R[j] = arr[mid + 1 + j];
+ 
+    long long indexOfSubArrayOne
+        = 0,
+        indexOfSubArrayTwo
+        = 0; 
+    long long indexOfMergedArray
+        = left;
+ 
+    while (indexOfSubArrayOne < subArrayOne
+           && indexOfSubArrayTwo < subArrayTwo) {
+        if (L[indexOfSubArrayOne]
+            <= R[indexOfSubArrayTwo]) {
+            arr[indexOfMergedArray]
+                = L[indexOfSubArrayOne];
+            indexOfSubArrayOne++;
+        }
+        else {
+            arr[indexOfMergedArray]
+                = R[indexOfSubArrayTwo];
+            indexOfSubArrayTwo++;
+        }
+        indexOfMergedArray++;
+    }
+    
+    while (indexOfSubArrayOne < subArrayOne) {
+        arr[indexOfMergedArray]
+            = L[indexOfSubArrayOne];
+        indexOfSubArrayOne++;
+        indexOfMergedArray++;
+    }
+    
+    while (indexOfSubArrayTwo < subArrayTwo) {
+        arr[indexOfMergedArray]
+            = R[indexOfSubArrayTwo];
+        indexOfSubArrayTwo++;
+        indexOfMergedArray++;
+    }
+}
+ 
+void mergeSort(vector<int>& arr, int const begin, int const end)
+{
+    if (begin >= end)
+        return; // Returns recursively
+ 
+    auto mid = begin + (end - begin) / 2;
+    mergeSort(arr, begin, mid);
+    mergeSort(arr, mid + 1, end);
+    merge(arr, begin, mid, end);
+}
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) { 
-        sort(nums1.begin(), nums1.end());
-        sort(nums2.begin(), nums2.end());
+        mergeSort(nums1, 0, nums1.size() - 1);
+        mergeSort(nums2, 0, nums2.size() - 1);
         int i = 0;
         int j = 0;
         vector<int> ans;

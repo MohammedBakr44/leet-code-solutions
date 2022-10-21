897558a530bf71615bfa84b7552d87852163c4b0
Mohamed Bakr <212284@csi.edu.eg>
2022-10-21 22:33:22 +0200

    Time: 12 ms (78.26%), Space: 7.5 MB (13.33%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        if(s == t) return true;
-        else return false;
+        map<char, int>fq;
+        if(s.length() != t.length()) return false;
+        for(int i = 0; i < s.length(); i++) {
+            fq[s[i]]++;
+            fq[t[i]]--;
+        }
+        for(auto it : fq) {
+            if(it.second != 0) return false;
+        }
+        return true;
     }
 };

b26a4786a09dcdc989a93ea764512138ef84e337
Mohamed Bakr <212284@csi.edu.eg>
2022-10-21 17:30:47 +0200

    Time: 515 ms (12.55%), Space: 80.4 MB (23.51%) - LeetHub

--- 219-contains-duplicate-ii/219-contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        map<int, vector<int>>idx;
+        map<int, int>mp;
         for(int i = 0; i < nums.size(); i++) {
-            idx[nums[i]].push_back(i+1);
-        }
-        for(auto it: idx) {
-            if(it.second.size() > 1) {
-                for(int i = 0; i <it.second.size() - 1; i++) {
-                    if(abs(it.second[i] - it.second[i+1]) <= k) return true;
-                }
+            if(mp.find(nums[i]) != mp.end()) {
+                if(abs(i - mp[nums[i]]) <= k) return true;
             }
+            mp[nums[i]] = i;
         }
         return false;
     }

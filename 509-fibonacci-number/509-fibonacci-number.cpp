3c1896dd5b8d07c7a4ec1ad6d869f92ac40a7e85
Mohamed Bakr <212284@csi.edu.eg>
2022-11-04 20:03:15 +0200

    Time: 0 ms (100.00%), Space: 6 MB (38.03%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int fib(int n) {
+        int ans[n+2];
+        ans[0] = 0;
+        ans[1] = 1;
+        for(int i = 2; i <= n; i++) {
+            ans[i] = ans[i-1] + ans[i-2];
+        }
+        return ans[n];
+    }
+};

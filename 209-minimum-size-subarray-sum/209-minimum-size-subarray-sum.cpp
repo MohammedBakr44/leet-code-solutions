734d2d898bb8db427863c62542486af0884fd9cc
Mohamed Bakr <212284@csi.edu.eg>
2022-12-16 20:02:01 +0200

    Time: 41 ms (91.75%), Space: 28.2 MB (54.40%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -6,10 +6,18 @@ public:
         int res = INT_MAX;
         int sum = 0;
         while(j < n) {
-            sum += nums[j++];
-            while(sum >= target) {
+            sum += nums[j];
+            j++; // add j to sum and then increment it
+            while(sum >= target) { // keep looping if sum >= target;
                 res = min(res, j - i);
-                sum -= nums[i++];
+                sum -= nums[i]; // decrease the value from right to get the minimum subarray
+                i++; // decrease i
+                // 7 [2, 3, 1, 2, 4, 3]
+                // sum = 2; condition not satisfied, sum += 3
+                // sum = 5, condition not satisfied, sum += 1 = 6;
+                // condition not satisified, sum += 2 = 8; condition satisfied;
+                // subtract nums[0] so it becomes 8 - 2 = 6;
+                // condition not satisfied so add j amd update j;
             }
         }
         return res%INT_MAX;

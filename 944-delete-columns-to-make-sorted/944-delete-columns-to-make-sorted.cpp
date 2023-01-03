732c867013363474a712dea65d2f2ff6d781b276
Mohamed Bakr <212284@csi.edu.eg>
2023-01-03 22:43:44 +0200

    Time: 50 ms (74.11%), Space: 12.1 MB (72.23%) - LeetHub

--- 944-delete-columns-to-make-sorted/944-delete-columns-to-make-sorted.cpp
+++ 944-delete-columns-to-make-sorted/944-delete-columns-to-make-sorted.cpp
@@ -4,10 +4,7 @@ public:
         int res = 0;
         int n = strs[0].length();
         for(int i = 0; i < n; i++) {
-            string temp = "";
-            string sorted = "";
             for(int j = 1; j < strs.size(); j++) {
-                //cout << strs[j-1][i] << " " << strs[j][i] << "\n";
                 if(strs[j][i] < strs[j-1][i]) {
                     res++;
                     break;

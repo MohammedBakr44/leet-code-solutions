5e23d33c3371addfa230c4b48be28dfaf4297136
Mohamed Bakr <212284@csi.edu.eg>
2023-01-04 23:51:02 +0200

    Time: 234 ms (84.20%), Space: 104.6 MB (34.15%) - LeetHub

---
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumRounds(vector<int>& tasks) {
+        map<int, int>mp;
+        int res = 0;
+        for(int i = 0; i < tasks.size(); i++) {
+            mp[tasks[i]]++;
+        }
+        for(auto &it : mp) {
+            if(it.second == 1) {
+                return -1;
+            }
+            if(it.second % 3 == 0) res += it.second / 3;
+            else res += it.second / 3 + 1;
+        }
+        return res;
+    }
+};

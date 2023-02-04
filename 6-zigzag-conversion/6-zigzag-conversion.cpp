12b4885e270e83690e46628c5ab7f7278bf10f27
Mohamed Bakr <212284@csi.edu.eg>
2023-02-04 02:25:46 +0200

    Time: 8 ms (83.84%), Space: 8.4 MB (69.43%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.cpp
+++ 6-zigzag-conversion/6-zigzag-conversion.cpp
@@ -3,8 +3,8 @@ public:
     string convert(string s, int numRows) {
         string res = "";
         if(numRows == 1) return s;
-        for(int i = 0; i < numRows; i++) {
-            int inc = 2 * (numRows - 1);
+        int inc = 2 * (numRows - 1);
+        for(int i = 0; i < numRows; i++) { 
             for(int j = i; j < s.length(); j += inc) {
                 res += s[j];
                 if(i > 0 && i < numRows - 1 && j + inc - 2 * i < s.length()) {

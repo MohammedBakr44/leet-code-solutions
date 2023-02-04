297313ac848ee14194c44c5dd728c32120be3adf
Mohamed Bakr <212284@csi.edu.eg>
2023-02-04 02:23:17 +0200

    Time: 7 ms (91.60%), Space: 8.3 MB (82.76%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.cpp
+++ 6-zigzag-conversion/6-zigzag-conversion.cpp
@@ -1,41 +1,17 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
+        string res = "";
         if(numRows == 1) return s;
-        int n = s.length();
-        int sections = ceil(n / (2 * numRows - 2.0));
-        int numCols = sections * (numRows - 1);
-        
-        vector<vector<char>> matrix(numRows, vector<char>(numCols, ' '));
-        int currRow = 0, currCol = 0;
-        int currStringIdx = 0;
-        
-        while(currStringIdx < n) {
-            // Move down
-            while(currRow < numRows && currStringIdx < n) {
-                matrix[currRow][currCol] = s[currStringIdx];
-                currRow++;
-                currStringIdx++;
-            }
-            
-            currRow -= 2;
-            currCol++;
-            // Move up and right
-            while(currRow > 0 && currCol < numCols && currStringIdx < n) {
-                matrix[currRow][currCol] = s[currStringIdx];
-                currRow--;
-                currCol++;
-                currStringIdx++;
-            }
-        }
-        string ans = "";
-        for(auto& row : matrix) {
-            for(auto& character : row) {
-                if(character != ' ') {
-                    ans += character;
+        for(int i = 0; i < numRows; i++) {
+            int inc = 2 * (numRows - 1);
+            for(int j = i; j < s.length(); j += inc) {
+                res += s[j];
+                if(i > 0 && i < numRows - 1 && j + inc - 2 * i < s.length()) {
+                    res += s[j + inc - 2 * i];
                 }
             }
         }
-        return ans;
+        return res;
     }
 };

62f7a84a583b33c928b545d8122d2a4312e1534d
Mohamed Bakr <212284@csi.edu.eg>
2023-11-20 05:57:05 +0200

    Time: 0 ms (100.00%), Space: 6.3 MB (94.99%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        std::string ans = "";
+        for(int i = 0; i < address.length(); i++)
+            address[i] == '.' ? ans += "[.]" : ans += address[i];
+        return ans;
+    }
+};
